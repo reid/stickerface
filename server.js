53a5a17b53bdbcb7ebd3980ba42e31e7bc3b33e1
Reid Burke <me@reidburke.com>
2010-05-06 18:25:00 -0700

    Add YUI3, nodejs-yui3, jsdom, etc.
    YUI3: Remove api, api-as, sandbox to reduce slug

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 require.paths.unshift("lib/express/lib");
+require.paths.unshift("lib/yui3");
+require.paths.unshift("lib");
+
 require("express");
 
+var YUI = require("node-yui3").YUI;
+
 get("/", function () {
-    return "Hello, Heroku!";
+    var express = this;
+    express.contentType("html");
+    YUI().use("nodejs-dom", function (Y) {
+        var document = Y.Browser.document;
+        var div = document.createElement("div");
+        div.innerHTML = "Hello, DOM!";
+        document.body.appendChild(div);
+        express.respond(200, document.body.outerHTML);
+    });
 });
 
 run(process.env.PORT || 8000, null);
