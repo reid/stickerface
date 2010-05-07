c9f511ba05300ccded9603a184dc827645a90d81
Reid Burke <me@reidburke.com>
2010-05-06 23:53:43 -0700

    Basic layout

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ require.paths.unshift("lib/express/lib");
 require.paths.unshift("lib/yui3");
 require.paths.unshift("lib");
 
-require("express");
-
 var YUI = require("node-yui3").YUI;
 
+require("express");
+
+configure(function(){
+    set("root", __dirname);
+});
+
 get("/", function () {
+    this.render("index.html.haml", {
+        locals : {
+            title : "Welcome",
+            env : process.env.EXPRESS_ENV
+        }
+    });
+});
+
+get("/yui", function () {
     var express = this;
     express.contentType("html");
     YUI().use("nodejs-dom", function (Y) {
