33cec20944d4a1c364040b8c9598315ad6f71b08
Reid Burke <me@reidburke.com>
2010-05-07 00:14:57 -0700

    Added working jsoauth library and test code

---
+++ query.js
@@ -0,0 +1,15 @@
+var query = require("./lib/jsoauth/api");
+var sys = require("sys");
+
+query.makeRequest(
+    "http://query.yahooapis.com/v1/yql",
+    [
+        "q=select%20%2A%20from%20social.updates.search%20where%20query%20%3D%20%22foo%22",
+        "format=json"
+    ],
+    function (error, data, response) {
+        if (error) return;
+        data = JSON.parse(data);
+        sys.debug(sys.inspect(data));        
+    }
+); 
